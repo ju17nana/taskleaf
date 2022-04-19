168943e14a5924ba278432e983a397e88b673c6c
ju17nana <h8chi.8@gmail.com>
2022-04-19 20:58:29 +0900

    Add NotNull Limit30

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_17_095552) do
+ActiveRecord::Schema.define(version: 2022_04_19_075240) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
